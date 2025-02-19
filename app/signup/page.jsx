244331d9db94fe932caced74597d27f572618ce6
kwamebaah1 <baahhprince79@gmail.com>
2025-02-19 02:54:29 +0000

    Updates

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -16,8 +16,8 @@ const signUpSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string().min(6, { message: "Password must be at least 6 characters" }),
   role: z.enum(["Customer", "Business Owner"], { message: "Please select an account type" }),
-  businessName: z.string().min(1, { message: "Business Name is required" }).optional(),
-  location: z.string().min(1, { message: "Location is required" }).optional(),
+  businessName: z.string().optional(),
+  location: z.string().optional(),
 }).refine((data) => {
   if (data.role === "Business Owner") {
     return !!data.businessName && !!data.location;
@@ -31,7 +31,6 @@ const signUpSchema = z.object({
 export default function SignUp() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-  
   const { register, handleSubmit, formState: { errors }, watch } = useForm({
     resolver: zodResolver(signUpSchema),
   });
@@ -40,28 +39,54 @@ export default function SignUp() {
 
   const onSubmit = async (data) => {
     setLoading(true);
-    const { name, email, password, role } = data;
 
-    const { error } = await supabase.auth.signUp({ email, password });
+    const { name, email, password, role, businessName, location } = data;
 
-    if (error) {
-        setLoading(false);
-        alert(error.message);
-        return;
-      }
-  
-      const { error: insertError } = await supabase
-        .from("users")
-        .insert([{ id: user.user.id, name, email, role }]);
-  
+    // Sign up user with authentication
+    const { data: authData, error: authError } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (authError) {
       setLoading(false);
-  
-      if (insertError) {
-        alert(insertError.message);
-      } else {
-        alert("Check your email for a confirmation link!");
-        router.push("/signin");
+      alert(authError.message);
+      return;
+    }
+
+    const userId = authData?.user?.id;
+    if (!userId) {
+      setLoading(false);
+      alert("User ID not found");
+      return;
+    }
+
+    try {
+      const { error: userInsertError } = await supabase.from("users").insert([
+        { id: userId, name, email, role, owner_id: role === "Business Owner" ? userId : null },
+      ]);
+
+      if (userInsertError) {
+        throw userInsertError;
       }
+
+      if (role === "Business Owner") {
+        const { error: restaurantInsertError } = await supabase.from("restaurants").insert([
+          { name: businessName, location, owner_id: userId },
+        ]);
+
+        if (restaurantInsertError) {
+          throw restaurantInsertError;
+        }
+      }
+
+      alert("Check your email for a confirmation link!");
+      router.push("/signin");
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -75,52 +100,52 @@ export default function SignUp() {
           <h1 className="text-center">SerenePath</h1>
         </div>
         <Card className="w-full max-w-md p-6 mx-4 sm:mx-0 mt-20 sm:mt-1">
-            <CardHeader>
+          <CardHeader>
             <CardTitle className="text-2xl">Sign Up</CardTitle>
-            </CardHeader>
-            <CardContent>
+          </CardHeader>
+          <CardContent>
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-                <div>
-                    <Input {...register("name")} type="text" placeholder="Name" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
-                    {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
-                </div>
-                <div>
-                    <Input {...register("email")} type="email" placeholder="Email" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
-                    {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
-                </div>
-                <div>
-                    <Input {...register("password")} type="password" placeholder="Password" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
-                    {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
-                </div>
-                <div>
-                    <label className="block text-sm font-medium">Account Type</label>
-                    <select {...register("role")} className="w-full text-sm sm:text-base px-3 py-2 border rounded bg-white text-gray-500">
-                        <option value="">Select Account Type</option>
-                        <option value="Customer">Customer</option>
-                        <option value="Business Owner">Business Owner</option>
-                    </select>
-                    {errors.role && <p className="text-red-500 text-sm">{errors.role.message}</p>}
-                </div>
-                {selectedRole === "Business Owner" && (
-                    <>
-                    <div>
-                        <Input {...register("businessName")} type="text" placeholder="Business Name" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
-                        {errors.businessName && <p className="text-red-500 text-sm">{errors.businessName.message}</p>}
-                    </div>
-                    <div>
-                        <Input {...register("location")} type="text" placeholder="Location" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
-                        {errors.location && <p className="text-red-500 text-sm">{errors.location.message}</p>}
-                    </div>
-                    </>
-                )}
-                <Button type="submit" className="w-full" disabled={loading}>
-                    {loading ? <Loader2 className="animate-spin" /> : "Sign Up"}
-                </Button>
+              <div>
+                <Input {...register("name")} type="text" placeholder="Name" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
+                {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
+              </div>
+              <div>
+                <Input {...register("email")} type="email" placeholder="Email" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
+                {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
+              </div>
+              <div>
+                <Input {...register("password")} type="password" placeholder="Password" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
+                {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
+              </div>
+              <div>
+                <label className="block text-sm font-medium">Account Type</label>
+                <select {...register("role")} className="w-full text-sm sm:text-base px-3 py-2 border rounded bg-white text-gray-500">
+                  <option value="">Select Account Type</option>
+                  <option value="Customer">Customer</option>
+                  <option value="Business Owner">Business Owner</option>
+                </select>
+                {errors.role && <p className="text-red-500 text-sm">{errors.role.message}</p>}
+              </div>
+              {selectedRole === "Business Owner" && (
+                <>
+                  <div>
+                    <Input {...register("businessName")} type="text" placeholder="Business Name" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
+                    {errors.businessName && <p className="text-red-500 text-sm">{errors.businessName.message}</p>}
+                  </div>
+                  <div>
+                    <Input {...register("location")} type="text" placeholder="Location" className="w-full text-sm sm:text-base py-2 sm:py-3 px-3 sm:px-4" />
+                    {errors.location && <p className="text-red-500 text-sm">{errors.location.message}</p>}
+                  </div>
+                </>
+              )}
+              <Button type="submit" className="w-full" disabled={loading}>
+                {loading ? <Loader2 className="animate-spin" /> : "Sign Up"}
+              </Button>
             </form>
             <p className="mt-4 text-center text-gray-400">
-                Already have an account? <a href="/signin" className="text-blue-400">Sign in</a>
+              Already have an account? <a href="/signin" className="text-blue-400">Sign in</a>
             </p>
-            </CardContent>
+          </CardContent>
         </Card>
       </div>
     </div>
