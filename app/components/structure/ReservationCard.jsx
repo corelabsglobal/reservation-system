0533b1505c696a71de7c3068b4cc1c08ebe2c222
kwamebaah1 <baahhprince79@gmail.com>
2025-07-13 15:59:25 +0000

    Update

--- app/components/structure/ReservationCard.jsx
+++ app/components/structure/ReservationCard.jsx
@@ -309,7 +309,6 @@ const ReservationCard = ({ res, markAsSeen, cancelReservation, markAsAttended, h
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center p-4 z-50"
-            style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0 }}
             onClick={() => setShowConfirmModal(false)}
           >
             <motion.div
